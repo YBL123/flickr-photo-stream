44f362a028bebcc00f8294456e8b10949bcb0108
Yarden Lawson <yardenlawson@gmail.com>
2021-02-01 11:03:13 +0000

    update

---
+++ src/Main.js
@@ -0,0 +1,21 @@
+// import './App.css';
+
+
+// get request
+// async await
+
+
+
+
+
+//map through data in the return to display all images + text
+
+function Main() {
+  return (
+    <div className="Main">
+      <h1>Flickr Photo Stream</h1>
+    </div>
+  );
+}
+
+export default Main;
